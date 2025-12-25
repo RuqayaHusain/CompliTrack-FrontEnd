6544609f86f6dd5acfc3fe92739bcf35882a9b66
zma8 <zainabmoosa880@gmail.com>
2025-12-25 18:05:27 +0300

    compliance task card done

--- src/components/ComplianceTask/ComplianceTaskCard.jsx
+++ src/components/ComplianceTask/ComplianceTaskCard.jsx
@@ -1,1 +1,27 @@
 import { Link } from "react-router";
+
+import { Link } from "react-router";
+
+const ComplianceTaskCard = ({ task, businessId }) => {
+    return (
+        <Link key={task.id} to={`/businesses/${businessId}/compliance-tasks/${task.id}`}>
+            <article>
+                <div>
+                    <h2>{task.title}</h2>
+                    <span>Status: {task.status}</span>
+                    <p>
+                        Due:{" "}
+                        {task.due_date
+                            ? new Date(task.due_date).toLocaleDateString()
+                            : "N/A"}
+                    </p>
+                </div>
+                <div>
+                    <p>View Details</p>
+                </div>
+            </article>
+        </Link>
+    );
+};
+
+export default ComplianceTaskCard;
