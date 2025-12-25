73972c3c11f283b140a847fa66e2f919ba529a39
zma8 <zainabmoosa880@gmail.com>
2025-12-25 18:20:01 +0300

    creating the compliance task list

--- src/components/ComplianceTask/ComplianceTaskList.jsx
+++ src/components/ComplianceTask/ComplianceTaskList.jsx
@@ -3,3 +3,16 @@ import { useNavigate, useParams } from "react-router";
 import { showAllComplianceTasks } from "../../services/complianceTaskService";
 import ComplianceTaskCard from "../ComplianceTaskCard/ComplianceTaskCard";
 
+const ComplianceTaskList = () => {
+    const { businessId } = useParams();
+    const [tasks, setTasks] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            const taskData = await showAllComplianceTasks(businessId);
+            setTasks(taskData);
+        };
+        if (businessId) fetchTasks();
+    }, [businessId]);
+    
