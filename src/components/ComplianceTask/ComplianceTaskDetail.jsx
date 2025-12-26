b475a7b6b8f991eed11ab10ef03de79998ad2610
zma8 <zainabmoosa880@gmail.com>
2025-12-26 08:08:09 +0300

    updating the compliance details adding the handle delete

--- src/components/ComplianceTask/ComplianceTaskDetail.jsx
+++ src/components/ComplianceTask/ComplianceTaskDetail.jsx
@@ -1,3 +1,23 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { showComplianceTask, deleteComplianceTask } from "../../services/complianceTaskService";
+
+const ComplianceTaskDetail = () => {
+    const { businessId, taskId } = useParams();
+    const navigate = useNavigate();
+    const [task, setTask] = useState(null);
+
+    useEffect(() => {
+        const fetchTask = async () => {
+            const taskData = await showComplianceTask(businessId, taskId);
+            setTask(taskData);
+        };
+        if (businessId && taskId) fetchTask();
+    }, [businessId, taskId]);
+
+    const handleDelete = async () => {
+        await deleteComplianceTask(businessId, taskId);
+        navigate(`/businesses/${businessId}`, { state: { activeTab: 'tasks' } });
+    };
+
+    if (!task) return <h3>Loading...</h3>;
