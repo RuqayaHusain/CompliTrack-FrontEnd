cc9e17ea26b993cb465cbc6bb01a27aa497682f0
zma8 <zainabmoosa880@gmail.com>
2025-12-26 08:04:31 +0300

    creating the compliance task details page

---
+++ src/components/ComplianceTask/ComplianceTaskDetail.jsx
@@ -0,0 +1,3 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { showComplianceTask, deleteComplianceTask } from "../../services/complianceTaskService";
